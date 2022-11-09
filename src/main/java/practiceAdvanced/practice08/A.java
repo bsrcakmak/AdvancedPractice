9110b666d0dc0d4b297d0fb896150a0918e021d2
Busra Cakmak <bsrckmk516@gmail.com>
2022-11-10 00:23:31 +0300

    Java Practice

---
+++ src/main/java/practiceAdvanced/practice08/A.java
@@ -0,0 +1,11 @@
+package practiceAdvanced.practice08;
+
+public class A {
+
+    int i=1;
+
+    public void yazdir(){
+        System.out.println(i);
+    }
+
+}
---
+++ src/main/java/practiceAdvanced/practice08/B.java
@@ -0,0 +1,13 @@
+package practiceAdvanced.practice08;
+
+public class B extends A{
+
+    int j=1;
+
+    public void yazdir(){
+        System.out.println(j);
+    }
+
+
+
+}
